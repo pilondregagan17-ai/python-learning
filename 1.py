bf529134f45456194ea6a69b85d1c31d5b927ba2
pilondregagan17-ai <pilondregagan17@gmail.com>
2026-03-13 18:53:43 +0530

    My first python program

---
+++ 1.py
@@ -0,0 +1,15 @@
+#My first python program:
+print("My first Python program")
+#Print Multiple lines:
+print("Hello World")
+print("Myself Gagan Somraj Pilondre ")
+print("Student of DY Patil International University")
+print("In B-Tech CSE Branch")
+#Print Numbers:
+print("Age:19")
+print("12th:70%")
+print("10th:75%")
+print("3.14")
+#Print with Variables:
+name = "Gagan"
+print(name)
