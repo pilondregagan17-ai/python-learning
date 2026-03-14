dfc86d26259960630483bd05b974325252fa1583
pilondregagan17-ai <pilondregagan17@gmail.com>
2026-03-14 17:42:37 +0530

    Renamed 1.py to my first python program.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+#Variable(printing name and age)
+name="Gagan"
+age=19
+marks=90.55
+age2=age
+print(name)
+print(age)
+print(marks)
+print(age2)
+print(type(name))
+print(type(age))
+print(type(marks))
