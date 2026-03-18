8b18e757c23a9e7cd8d42f14cec3e32269001c9c
pilondregagan17-ai <pilondregagan17@gmail.com>
2026-03-18 18:48:13 +0530

    added Areas.py

---
+++ Areas.py
@@ -0,0 +1,17 @@
+# Area of square
+a=float(input("Enter length of Side:"))
+area=a*a
+print("Area of Square:",area)
+print("")
+
+# Area of rectangle
+l=float(input("Enter the length:"))
+b=float(input("Enter the breadth:"))
+area=l*b
+print("Area of rectangle:",area)
+print("")
+
+# Area of circle
+r=float(input("Enter Radius:"))
+area= 3.14*r*r
+print("Area of circle:",area)
