55b60682ae20667cd1f3836481c4892665450279
pilondregagan17-ai <pilondregagan17@gmail.com>
2026-03-18 18:35:01 +0530

    Added operator.py

---
+++ operators.py
@@ -0,0 +1,29 @@
+#Arithematic Operator
+a=10
+b=5
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Remainder:",a%b)
+print("Power:",a**b)
+print("")
+
+# Comparision Operator
+a=13
+b=45
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print("")
+
+#Logical Operators
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
