a8735eddbab79f5670baede5b37d4dc6272ec46a
pilondregagan17-ai <pilondregagan17@gmail.com>
2026-03-20 18:53:51 +0530

    Decision making.py added

---
+++ Decision Making.py
@@ -0,0 +1,24 @@
+#If statement
+a=int(input("Enter Your Age:"))
+if a>=18:
+    print("You Can Vote")
+print("")
+
+#If-else statement
+a=int(input("Enter Your Age"))
+if a>+18:
+    print("You are ELigible to Vote")
+else:
+    print("You are not Eligible")
+print("")
+
+#if-elif-else statement
+a=float(input("Enter your Marks"))
+if a>=90:
+    print("Grade:A")
+elif a>=60:
+    print("Grade:B")
+elif a>=35:
+    print("Grade:C")
+else:
+    print("Fail")
